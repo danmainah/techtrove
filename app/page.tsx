6cc0f4c3b007848444e145e439976e464c8c7e0e
Daniel Maina <danielmainawaweru@gmail.com>
2025-04-28 18:35:02 +0300

    setup the app and database connection with prisma

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome to Tech Trove</h2>
+      <p>This is a place to find the best tech products and deals</p>
+    </div>
+  );
+}
